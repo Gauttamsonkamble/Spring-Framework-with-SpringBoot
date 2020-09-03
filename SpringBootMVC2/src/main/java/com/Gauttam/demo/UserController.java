93fde101bc8f208e7cb2f9b66a2081f030ba4df7
Gauttam Sonkamble <46339795+Gauttamsonkamble@users.noreply.github.com>
2020-09-04 02:56:09 +0530

    Add files via upload

--- SpringBootMVC2/src/main/java/com/Gauttam/demo/UserController.java
+++ SpringBootMVC2/src/main/java/com/Gauttam/demo/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Gauttam.demo.dao.UserRepo;
@@ -28,6 +29,14 @@ public class UserController
 	public User user(@PathVariable("uid") int uid) 
 	{
 		User user = repo.findById(uid).orElse(new User(0,""));
+		
+		return user;
+	}
+	
+	@PostMapping("User")
+	public User addUser(User user)
+	{
+		repo.save(user);
 		
 		return user;
 	}
