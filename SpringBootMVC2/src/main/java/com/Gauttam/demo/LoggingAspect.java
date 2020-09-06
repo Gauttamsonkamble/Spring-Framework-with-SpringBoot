c08952f2fa3f093152098cb3177c6c29277c1116
Gauttam Sonkamble <46339795+Gauttamsonkamble@users.noreply.github.com>
2020-09-06 20:58:03 +0530

    Add files via upload

--- SpringBootMVC2/src/main/java/com/Gauttam/demo/LoggingAspect.java
+++ SpringBootMVC2/src/main/java/com/Gauttam/demo/LoggingAspect.java
@@ -2,15 +2,24 @@ package com.Gauttam.demo;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect 
 {
+
+	 
+	private static final Logger log = LoggerFactory.getLogger(LoggingAspect.class);
+
+	
+	
 	@Before("execution(public * com.Gauttam.demo.UserController.getUsers())")
 	public void log()
 	{
-		System.out.println("getUsers Method called");
+	    log.info("getUsers Method called from aspect");
+		
 	}
 }
